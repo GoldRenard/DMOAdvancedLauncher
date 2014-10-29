1716ded04a1113abe23b0fa9107855fcd3dac250
Renard Gold <goldrenard@gmail.com>
2014-10-30 01:09:55 +0300

    fix AutoScrollBehavior snapping feature

--- AdvancedLauncher/Controls/AutoScrollBehavior.cs
+++ AdvancedLauncher/Controls/AutoScrollBehavior.cs
@@ -7,7 +7,6 @@ namespace AdvancedLauncher.Controls {
     public class AutoScrollBehavior : Behavior<ScrollViewer> {
         private ScrollViewer Target = null;
         private double Height = 0.0d;
-        private double PinningHeight = 0.0d;
 
         protected override void OnAttached() {
             base.OnAttached();
@@ -16,16 +15,15 @@ namespace AdvancedLauncher.Controls {
         }
 
         private void OnLayoutUpdated(object sender, EventArgs e) {
-            if (this.Target.ExtentHeight != Height && PinningHeight == this.Target.ContentVerticalOffset + this.Target.ViewportHeight) {
+            if (this.Target.ExtentHeight != Height
+                && this.Height - this.Target.ContentVerticalOffset <= this.Target.ViewportHeight) {
                 this.Target.ScrollToVerticalOffset(this.Target.ExtentHeight);
                 this.Height = this.Target.ExtentHeight;
             }
-            this.PinningHeight = this.Target.ExtentHeight;
         }
 
         protected override void OnDetaching() {
             base.OnDetaching();
-
             if (this.Target != null) {
                 this.Target.LayoutUpdated -= new EventHandler(OnLayoutUpdated);
             }

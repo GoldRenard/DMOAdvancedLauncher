e85a6e1e88970937efb167eed53471b2167d148c
Renard Gold <goldrenard@gmail.com>
2014-10-30 00:46:51 +0300

    2014

--- DMOLibrary/DMOLibrary.cs
+++ DMOLibrary/DMOLibrary.cs
@@ -1,6 +1,6 @@
 ﻿// ======================================================================
 // DMOLibrary
-// Copyright (C) 2013 Ilya Egorov (goldrenard@gmail.com)
+// Copyright (C) 2014 Ilya Egorov (goldrenard@gmail.com)
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by

b59ca3370aca5dc5e5f32d14d34474c97cc4ea92
Renard Gold <goldrenard@gmail.com>
2015-03-25 15:38:20 +0300

    add version xml

---
+++ AdvancedLauncher/Environment/Containers/RemoteVersion.cs
@@ -0,0 +1,100 @@
+﻿// ======================================================================
+// DIGIMON MASTERS ONLINE ADVANCED LAUNCHER
+// Copyright (C) 2015 Ilya Egorov (goldrenard@gmail.com)
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// ======================================================================
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace AdvancedLauncher.Environment.Containers {
+
+    [XmlType(TypeName = "RemoteVersion")]
+    public class RemoteVersion {
+
+        [XmlElement("Version")]
+        public string VersionString {
+            set;
+            get;
+        }
+
+        [XmlIgnore]
+        public Version Version {
+            set {
+                VersionString = value.ToString();
+            }
+            get {
+                if (string.IsNullOrEmpty(VersionString)) {
+                    return null;
+                }
+                return new Version(VersionString);
+            }
+        }
+
+        [XmlElement]
+        public string DownloadUrl {
+            set;
+            get;
+        }
+
+        [XmlIgnore]
+        public string ChangeLog {
+            set;
+            get;
+        }
+
+        [XmlText]
+        public XmlNode[] CDataContent {
+            get {
+                var dummy = new XmlDocument();
+                return new XmlNode[] { dummy.CreateCDataSection(System.Environment.NewLine + ChangeLog + System.Environment.NewLine) };
+            }
+            set {
+                if (value == null) {
+                    ChangeLog = null;
+                    return;
+                }
+
+                if (value.Length != 1) {
+                    throw new InvalidOperationException(
+                        String.Format(
+                            "Invalid array length {0}", value.Length));
+                }
+
+                ChangeLog = value[0].Value.Trim();
+            }
+        }
+
+        public static RemoteVersion DeSerialize(string filepath) {
+            RemoteVersion db = new RemoteVersion();
+            if (File.Exists(filepath)) {
+                XmlSerializer reader = new XmlSerializer(typeof(RemoteVersion));
+                using (var file = new StreamReader(filepath)) {
+                    db = (RemoteVersion)reader.Deserialize(file);
+                }
+            }
+            return db;
+        }
+
+        public static void Save(RemoteVersion instance) {
+            XmlSerializer writer = new XmlSerializer(typeof(RemoteVersion));
+            using (var file = new StreamWriter(@"D:\version.xml")) {
+                writer.Serialize(file, instance);
+            }
+        }
+    }
+}

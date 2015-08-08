6937840ba68ffebf0901be08cf238ddf82812508
Renard Gold <goldrenard@gmail.com>
2015-08-08 21:59:24 +0300

    use real release key

--- DecryptReleaseKey.ps1
+++ DecryptReleaseKey.ps1
@@ -1,9 +1,6 @@
 [Reflection.Assembly]::LoadWithPartialName("System.Security");
 
-Get-Location;
-
 function Encrypt-File($File, $EncryptedFile, $Salt, $Passphrase, $Init) {
-Write-Host  test $Passphrase;
 	$pass = [Text.Encoding]::UTF8.GetBytes($Passphrase);
 	$salt = [Text.Encoding]::UTF8.GetBytes($Salt);
 	$init = [Text.Encoding]::UTF8.GetBytes($Init);
@@ -51,17 +48,10 @@ $inSalt       = $env:SNK_SEC_SALT;
 $inPassphrase = $env:SNK_SEC_PASSPHRASE;
 $inInit       = $env:SNK_SEC_INIT;
 
-#$inSalt       = "mysalt";
-#$inPassphrase = "mypassphrase";
-#$inInit       = "mysecinit";
-
 if ([string]::IsNullOrEmpty($inSalt) -or [string]::IsNullOrEmpty($inSalt) -or [string]::IsNullOrEmpty($inSalt)) {
     Write-Host "Unknown encryption data, skipping release key decryption...";
     exit;
 }
 
 Decrypt-File "C:\Projects\DMOAdvancedLauncher\signkey.release.enc" "C:\Projects\DMOAdvancedLauncher\signkey.snk" $inSalt $inPassphrase $inInit;
-
-$md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider;
-$hash = [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes("C:\Projects\DMOAdvancedLauncher\signkey.snk")));
-Write-Host Decrypted MD5: $hash;
+Write-Host "Release key has been decrypted";

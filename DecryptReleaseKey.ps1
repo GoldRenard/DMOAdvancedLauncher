30e28a793a3371de5e2c2ece0b10ff629c372837
Renard Gold <goldrenard@gmail.com>
2015-08-08 20:54:15 +0300

    appveyor key decryption

---
+++ DecryptReleaseKey.ps1
@@ -0,0 +1,64 @@
+[Reflection.Assembly]::LoadWithPartialName("System.Security");
+
+function Encrypt-File($File, $EncryptedFile, $Salt, $Passphrase, $Init) {
+Write-Host  test $Passphrase;
+	$pass = [Text.Encoding]::UTF8.GetBytes($Passphrase);
+	$salt = [Text.Encoding]::UTF8.GetBytes($Salt);
+	$init = [Text.Encoding]::UTF8.GetBytes($Init);
+
+	$r     = new-Object  System.Security.Cryptography.RijndaelManaged;
+	$r.Key = (new-Object Security.Cryptography.PasswordDeriveBytes $pass, $salt, "SHA1", 5).GetBytes(32);
+	$r.IV  = (new-Object Security.Cryptography.SHA1Managed).ComputeHash($init)[0..15];
+
+	$c         = $r.CreateEncryptor();
+    $inStream  = new-Object IO.FileStream($File, "Open");
+    $outStream = new-Object IO.FileStream($EncryptedFile, "Create");
+    $cs        = new-Object Security.Cryptography.CryptoStream $outStream, $c, "Write";
+
+    $inStream.CopyTo($cs);
+
+    $cs.Close();
+    $inStream.Close();
+    $outStream.Close();
+    $r.Clear();
+}
+
+function Decrypt-File($EncryptedFile, $File, $Salt, $Passphrase, $Init) {
+	$pass = [Text.Encoding]::UTF8.GetBytes($Passphrase);
+	$salt = [Text.Encoding]::UTF8.GetBytes($Salt);
+	$init = [Text.Encoding]::UTF8.GetBytes($Init);
+
+	$r     = new-Object  System.Security.Cryptography.RijndaelManaged;
+	$r.Key = (new-Object Security.Cryptography.PasswordDeriveBytes $pass, $salt, "SHA1", 5).GetBytes(32);
+	$r.IV  = (new-Object Security.Cryptography.SHA1Managed).ComputeHash($init)[0..15];
+
+	$c         = $r.CreateDecryptor();
+    $inStream  = new-Object IO.FileStream($EncryptedFile, "Open");
+    $outStream = new-Object IO.FileStream($File, "Create");
+    $cs        = new-Object Security.Cryptography.CryptoStream $inStream, $c, "Read";
+
+    $cs.CopyTo($outStream);
+
+    $cs.Close();
+    $inStream.Close();
+    $outStream.Close();
+    $r.Clear();
+}
+
+$inSalt       = $env:SNK_SEC_SALT;
+$inPassphrase = $env:SNK_SEC_PASSPHRASE;
+$inInit       = $env:SNK_SEC_INIT;
+
+#$inSalt       = "mysalt";
+#$inPassphrase = "mypassphrase";
+#$inInit       = "mysecinit";
+
+Write-Host $inSalt;
+Write-Host $inPassphrase;
+Write-Host $inInit;
+
+Decrypt-File "signkey.release.enc" "signkey.snk" $inSalt $inPassphrase $inInit;
+
+$md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider;
+$hash = [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes("signkey.snk")));
+Write-Host Decrypted MD5: $hash;
